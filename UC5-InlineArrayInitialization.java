7fd9a4c412fad2b1f1550702a46939e9ad7f24a7
sanyuktadahiwal <sanyuktadahiwal7@gmail.com>
2026-03-14 13:38:38 +0530

    Render OOPS as Banner using Inline Array Initialization

---
+++ UC5-InlineArrayInitialization.java
@@ -0,0 +1,14 @@
+public class UC5-InlineArrayInitialization {
+	public static void main(String[] args){
+		String[] lines = {
+            String.join("", "   *   ", "   *   ", " ***   ", " ***  "),
+            String.join("", " *   * ", " *   * ", " *   * ", " * "),
+            String.join("", " *   * ", " *   * ", " ***   ", "  **  "),
+            String.join("", " *   * ", " *   * ", " *     ", "    *  "),
+            String.join("", "   *   ", "   *   ", " *     ", " *** "),
+        };
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
